7521173b760ef98587560460fcb1523896188916
KurtSchneider0 <96206284+KurtSchneider0@users.noreply.github.com>
2022-01-13 08:51:48 +0100

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 export class Todo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  
   render() {
     return( 
       <div style={{display: "flex", justifyContent: "center"}}>
